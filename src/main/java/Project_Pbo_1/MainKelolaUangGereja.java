88f65043f30909fd1932c2c32819d60e885b9659
Gorthdzx <krislaksana123@gmail.com>
2023-06-05 20:49:10 +0700

    done

--- src/main/java/Project_Pbo_1/MainKelolaUangGereja.java
+++ src/main/java/Project_Pbo_1/MainKelolaUangGereja.java
@@ -1,6 +1,5 @@
 package Project_Pbo_1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -11,122 +10,76 @@ public class MainKelolaUangGereja {
         Paroki paroki = new Paroki();
         paroki.setNamaGereja("Gereja Katolik Santa Perawan Maria di Fatima");
         PemasukanGereja[] pemasukanGerejas;
-        pemasukanGerejas = new PemasukanGereja[2];
+        pemasukanGerejas = new PemasukanGereja[0];
         Sekretariat_Administrasi_Gereja sekretariatGereja = new Sekretariat_Administrasi_Gereja();
         int pilih, choice;
         Scanner input = new Scanner(System.in);
-        boolean isRunning = false;
+        boolean isRunning;
+
 
         do {
+            PemasukanGereja[] tampung;
+            tampung = Arrays.copyOf(pemasukanGerejas, pemasukanGerejas.length + 1);
 
-            System.out.println("Apakah anda ingin berdonasi  ");
-            System.out.println(" 1. YA  ");
-            System.out.println(" 2. TIDAK ");
+
+            System.out.println("1. Donasi Untuk pembangunan Gereja");
+            System.out.println("2. Kolekte gereja");
+            System.out.println("3. Pemasukan Uang Parkir gereja");
+            System.out.print("Pilih : ");
+            choice = input.nextInt();
+
+            if (choice == 1) {
+                System.out.print("Nama : ");
+                String name = input.next();
+                tampung[pemasukanGerejas.length] = new PemasukanDonatur();
+                ((PemasukanDonatur) tampung[pemasukanGerejas.length]).setNamaDonatur(name);
+                ((PemasukanDonatur) tampung[pemasukanGerejas.length]).inputDonasi();
+            } else if (choice == 2) {
+                System.out.println("Nama Lingkungan : ");
+                String name = input.next();
+
+                System.out.println("Penanggung Jawab : ");
+                String pj = input.next();
+                tampung[pemasukanGerejas.length] = new PemasukanTeksMisa();
+                ((PemasukanTeksMisa) tampung[pemasukanGerejas.length]).inputKolekte();
+                ((PemasukanTeksMisa) tampung[pemasukanGerejas.length]).setNamaLinkungan(name);
+                ((PemasukanTeksMisa) tampung[pemasukanGerejas.length]).setPenangggugJawab(pj);
+            } else if (choice == 3) {
+                System.out.println("Nama Lingkungan : ");
+                String name = input.next();
+                tampung[pemasukanGerejas.length] = new PemasukanUangParkir();
+                ((PemasukanUangParkir) tampung[pemasukanGerejas.length]).setNamaLinkungan(name);
+                ((PemasukanUangParkir) tampung[pemasukanGerejas.length]).totalParkir();
+            } else {
+                System.out.println("Invalid");
+            }
+            pemasukanGerejas = tampung;
+            sekretariatGereja.setPemasukanGerejas(pemasukanGerejas);
+            System.out.println("mau memasukan data lagi : ");
+            String cek = input.next();
+
+            isRunning = cek.equalsIgnoreCase("ya");
+        } while (choice < 1 || choice > 3 || isRunning);
+
+
+        divider();
+        System.out.println("Informasi Daftar uang pemasukan di " + paroki.getNamaGereja());
+        System.out.println("Ketua Sekretariat  : " + sekretariatGereja.getKetuaSekretaris());
+        divider();
+        do {
+            System.out.println(" 1. Cetak Daftar Pemasukan   ");
+            System.out.println(" 2. Keluar ");
             System.out.print("Pilih : ");
             pilih = input.nextInt();
 
             if (pilih == 1) {
-
-                for (int i = 0; i < pemasukanGerejas.length; i++) {
-                    do {
-                        System.out.println("benar ");
-                        System.out.println("1. Donasi Untuk pembangunan Gereja");
-                        System.out.println("2. Kolekte gereja");
-                        System.out.println("3. Pemasukan Uang Parkir gereja");
-                        System.out.print("Pilih : ");
-                        choice = input.nextInt();
-
-                        if (choice == 1) {
-                            System.out.print("Nama : ");
-                            String name = input.next();
-                            pemasukanGerejas[i] = new PemasukanDonatur();
-                            ((PemasukanDonatur) pemasukanGerejas[i]).setNamaDonatur(name);
-                            ((PemasukanDonatur) pemasukanGerejas[i]).inputDonasi();
-                        } else if (choice == 2) {
-                            pemasukanGerejas[i] = new PemasukanTeksMisa();
-                            ((PemasukanTeksMisa) pemasukanGerejas[i]).jumlahBeliTeksMisa();
-                        } else if (choice == 3) {
-                            pemasukanGerejas[i] = new PemasukanUangParkir();
-                            ((PemasukanUangParkir) pemasukanGerejas[i]).totalParkir();
-                        } else {
-                            System.out.println("Invalid Option ");
-                        }
-                    } while (choice < 1 || choice > 3);
-                }
-
-                break;
+                sekretariatGereja.cetakPemasukanGereja();
             } else if (pilih == 2) {
-                System.out.println("Terimakasih");
-                isRunning = false;
-            } else {
-                System.out.println("Input 1 - 2 ");
-                isRunning = true;
-            }
-        } while (pilih < 1 || pilih > 2);
-
-        /*
-        nambah donasi
-         */
-                PemasukanGereja [] temp;
-                temp = Arrays.copyOf(pemasukanGerejas , pemasukanGerejas.length + 1);
-                pemasukanGerejas  = temp;
-
-                ArrayList<PemasukanGereja>pemasukanGerejas1 = new ArrayList<>();
-
-
-
-
-        /*
-        OUTPUT
-         */
-
-        System.out.println("Apakah anda ingin mencetak informasi Daftar Donasi : ? ");
-        System.out.println(" 1. YA  ");
-        System.out.println(" 2. TIDAK ");
-        System.out.print("Pilih : ");
-        pilih = input.nextInt();
-
-        sekretariatGereja.setPemasukanGerejas(pemasukanGerejas);
-
-        do {
-            if (pilih == 1) {
-                do {
-                    divider();
-                    System.out.println("Informasi Daftar uang pemasukan di " + paroki.getNamaGereja());
-                    System.out.println("Ketua Sekretariat  : " + sekretariatGereja.getKetuaSekretaris());
-                    divider();
-                    System.out.println(" 1. Cetak Daftar Pemasukan   ");
-                    System.out.println(" 2. Cetak Pemasukan Parki  gereja ");
-                    System.out.println(" 3. Cari jenis Donasi");
-                    System.out.println(" 4. Keluar Program");
-                    System.out.print("Pilih : ");
-                    pilih = input.nextInt();
-
-
-                    if (pilih == 1) {
-                        sekretariatGereja.cetakPemasukanDonatur();
-                    } else if (pilih == 2) {
-                        sekretariatGereja.cetakPemasukanParkir();
-                    } else if (pilih == 3) {
-                        System.out.println("Cari jenis donasi ");
-                        String c = input.next();
-                        sekretariatGereja.cariJenisDonasi(c);
-                    } else if (pilih == 4) {
-                        System.out.println("Terimakasih, berkah dalem....");
-                        System.out.println("Menutup program..");
-                        System.exit(0);
-                    } else {
-                        System.out.println("Invalid");
-                        isRunning = false;
-                    }
-                } while (!(isRunning));
-            } else if (pilih == 2) {
-                System.out.println("Terimakasih , Berkah dalem .....");
+                System.out.println("Terimakasih , Berkah Dalem....");
                 System.exit(0);
             } else {
-                System.out.println("Invalid ");
-                isRunning = false;
+                System.out.println("Invalid");
             }
-        } while (!(isRunning));
+        } while (true);
     }
 }
